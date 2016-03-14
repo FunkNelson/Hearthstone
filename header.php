0a41a06940b72e470d3b35f6d9b2e1b6ffec8bf2
FunkNelson <rossmb@gmail.com>
2016-03-13 21:35:45 -0700

    Added files via upload
    header php

---
+++ header.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+	<title>FunkNelson's Hearthstone Thing</title>
+	<link href="funk.css" rel="stylesheet">
+  </head>
+  <body>
+	<div id='header'>
+	  <table align="center">
+		<tr>
+		  <td align="left"><img src="cagecat.png"></td>
+		  <td align="center"><h1>FunkNelson's Hearthstone Thing</h1></td>			
+		  <td align="right"><img src="cagecat.png"></td>
+		</tr>
+	  <table>
+	</div>
+	<div id='menu'>  
+	  <table align="center">	  
+		<tr>
+		  <td align="center"><a href="index.html">Home</a></td>
+		  <td align="center"><a href="viewdecks.php">Decks</a></td>	  
+		  <td align="center"><a href="vhttp://www.icy-veins.com/hearthstone/">Icy-Veins</a></td>
+		</tr>
+	  </table>
+	</div>
---
+++ submitdeck.php
@@ -0,0 +1,69 @@
+<html>
+  <head>
+    <title>Hearthstone Thing</title>
+    <link href="funk.css" rel="stylesheet">
+  </head>
+  <body>
+	<?php
+		require('header.php');
+	?>
+    <form action="doathing.php" method="post">
+	  <br /><br />
+      <table border="0" class="center">
+        <tr>
+	      <td align="right">Deck Name</td>
+	      <td align="center"><input type="text" name="deckname" size="16" maxlength="16" /></td>
+        </tr>
+        <tr>
+	      <td align="right">Style</td>
+	      <td><select name="style">
+		    <option value = "nothing">Choose</option>
+		    <option value = "a">rush</option>
+		    <option value = "b">midrange</option>
+		    <option value = "c">control</option>
+		    <option value = "d">mill</option>
+		    <option value = "e">fatigue</option>
+		    <option value = "f">other</option>
+			</select>
+		  </td>
+        </tr>
+        <tr>
+	      <td align="right">Select your class</td>
+	      <td><select name="find">
+		    <option value = "nothing">Choose</option>
+		    <option value = "a">Druid</option>
+		    <option value = "b">Hunter</option>
+		    <option value = "c">Mage</option>
+		    <option value = "d">Paladin</option>
+		    <option value = "e">Priest</option>
+		    <option value = "f">Rogue</option>
+		    <option value = "g">Shaman</option>
+		    <option value = "h">Warlock</option>
+		    <option value = "i">Warrior</option>
+			</select>
+		  </td>
+        </tr>	
+		<tr>
+		  <td align="right">Minion count</td>
+		  <td><input type="number" name="minions" min="0" max="30" /></td>
+		</tr>
+		<tr>
+		  <td align="right">Spell count</td>
+		  <td><input type="number" name="spells" min="0" max="30" /></td>
+		</tr>
+		<tr>
+		  <td align="right">Weapon count</td>
+		  <td><input type="number" name="weapons" min="0" max="30" /></td>
+		</tr>
+        <tr>
+	      <td colspan="2" align="center"><input type="submit" value="Touch this." /></td>
+        </tr>
+      </table>
+    </form>
+	<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/GVG_110.png">
+	<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/AT_132.png">
+	<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/EX1_016.png">
+	<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/EX1_572.png">
+	<img src="http://wow.zamimg.com/images/hearthstone/cards/enus/medium/FP1_030.png">
+  </body>
+</html>	
