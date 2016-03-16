1d2f7a655c8b22ccb197b513e3f5779bba93c83f
FunkNelson <rossmb@gmail.com>
2016-03-15 19:10:00 -0700

    Added files via upload
    extended lookup query

--- cardlookup.php
+++ cardlookup.php
@@ -12,28 +12,84 @@
 	<form action="lookupresults.php" method="post">
 	  <table border="0" class="center">
 		<tr>
-		  <td align="right">Name contains: </td>
+		  <td align="right">Name </td>
+		  <td align="center">contains</td>
 		  <td align="left"><input type="searchterm" name="searchterm" size="20" maxlength="20" /></td>
 		</tr>
 		<tr>
-		  <td align="right">Class type: </td>
+		  <td align="right">Class </td>
+		  <td align="center">is</td>
 		  <td><select name="hero">
 		    <option value = "all">All</option>
-			<option value = "x">Neutral</option>
-			<option value = "a">Druid</option>
-		    <option value = "b">Hunter</option>
-		    <option value = "c">Mage</option>
-		    <option value = "d">Paladin</option>
-		    <option value = "e">Priest</option>
-		    <option value = "f">Rogue</option>
-		    <option value = "g">Shaman</option>
-		    <option value = "h">Warlock</option>
-		    <option value = "i">Warrior</option>
+			<option value = "neutral">Neutral</option>
+			<option value = "druid">Druid</option>
+		    <option value = "hunter">Hunter</option>
+		    <option value = "mage">Mage</option>
+		    <option value = "paladin">Paladin</option>
+		    <option value = "priest">Priest</option>
+		    <option value = "rogue">Rogue</option>
+		    <option value = "shaman">Shaman</option>
+		    <option value = "warlock">Warlock</option>
+		    <option value = "warrior">Warrior</option>
    		    <select>
 		  </td>
 		</tr>
+		<tr>
+		  <td align="right">Race </td>
+		  <td align="center">is</td>
+		  <td><select name="race">
+		    <option value = "all">All</option>
+			<option value = "beast">Beast</option>
+			<option value = "demon">Demon</option>
+		    <option value = "dragon">Dragon</option>
+		    <option value = "mech">Mech</option>
+		    <option value = "murloc">Murloc</option>
+		    <option value = "pirate">Pirate</option>
+		    <option value = "totem">Totem</option>
+   		    <select>
+		  </td>
+		</tr>
+		<tr>
+		  <td align="right">Mana </td>
+		  <td align="center"><select name="mana_operand">
+			<option value = "=">=</option>
+		    <option value = "<">&lt;</option>
+			<option value = ">">&gt;</option>
+			<option value = "<=">&lt;=</option>
+			<option value = ">=">&gt;=</option>
+			<option value = "!=">!=</option>
+   		    <select>
+		  </td>
+		  <td align="left"><input type="number" name="mana" min="0" max="50" /></td>
+		</tr>  
+		<tr>
+		  <td align="right">Attack </td>
+		  <td align="center"><select name="attack_operand">
+			<option value = "=">=</option>
+		    <option value = "<">&lt;</option>
+			<option value = ">">&gt;</option>
+			<option value = "<=">&lt;=</option>
+			<option value = ">=">&gt;=</option>
+			<option value = "!=">!=</option>
+   		    <select>
+		  </td>
+		  <td><input type="number" name="attack" min="0" max="50" /></td>
+		</tr>  
+		<tr>
+		  <td align="right">Health </td>
+		  <td align="center"><select name="health_operand">
+			<option value = "=">=</option>
+		    <option value = "<">&lt;</option>
+			<option value = ">">&gt;</option>
+			<option value = "<=">&lt;=</option>
+			<option value = ">=">&gt;=</option>
+			<option value = "!=">!=</option>
+   		    <select>
+		  </td>
+		  <td><input type="number" name="health" min="0" max="50" /></td>
+		</tr>
         <tr>
-	      <td colspan="2" align="center"><input type="submit" value="Look up" /></td>
+	      <td colspan="3" align="center"><input type="submit" value="Find all the cards" /></td>
         </tr>		
 	  </table>
 	</form>
--- cardquery.php
+++ cardquery.php
@@ -1,20 +1,28 @@
 <?php
 $searchtype = $_POST['searchtype'];
 $searchterm = $_POST['searchterm'];
-$hero_choice = $_POST['hero'];
-$hero = "";
+$hero = $_POST['hero'];
+$race = $_POST['race'];
+$mana = $_POST['mana'];
+$attack = $_POST['attack'];
+$health = $_POST['health'];
 
-$class_query = true;
+$mana_operand = $_POST['mana_operand'];
+$attack_operand = $_POST['attack_operand'];
+$health_operand = $_POST['health_operand'];
+
+
 
 //DB LOGIN
 require('db_connection_config.php');
 
-
+/*
 if (!$searchterm)
 {
 	echo "You didn't search for anything.  Try more.";
 	exit;
 }
+*/
 
 if (!get_magic_quotes_gpc())
 {
@@ -30,94 +38,62 @@ if (mysqli_connect_errno())
 	exit;
 }
 
-switch($hero_choice)
-{
-	case "a":
-		$class_type = "druid";
-		break;
-	case "b":
-		$class_type = "hunter";
-		break;
-	case "c":
-		$class_type = "mage";
-		break;
-	case "d":
-		$class_type = "paladin";
-		break;
-	case "e":
-		$class_type = "priest";
-		break;
-	case "f":
-		$class_type = "rogue";
-		break;
-	case "g":
-		$class_type = "shaman";
-		break;
-	case "h":
-		$class_type = "warlock";
-		break;
-	case "i":
-		$class_type = "warrior";
-		break;
-	case "x":
-		$class_type = "neutral";
-		break;
-	case "all":
-		$class_query = false;
-		break;
-	default:
-		$class_query = false;
-		break;
-}
 
+$query = construct_query($hero, $searchterm, $mana, $mana_operand, $attack, $attack_operand, $health, $health_operand, $race);
 
-//Create SQL query
-if (strlen($searchterm) > 0)
-{
-	$query = "select name, hero, type, quality, race, expansion, mana, attack, health from cards where name like '%$searchterm%'";
-	
-	if ($class_query)
-	{
-		$query .= " and hero = '$class_type'";
-	}
-}
-else
-{
-	$query = "select name, type, quality, race, expansion, mana, attack, health from cards where hero = '$class_type'";
-}
-
-$query .= "and collectable = 1 and type in ('minion', 'spell', 'weapon')";
+//debug query
+//echo $query;
 
 $result = $db->query($query);
-
-
 construct_results_table($result);
-
-//$num_results = $result->num_rows;
-
-//echo "<p>$num_results cards found  |  <a href=\"cardlookup.php\">Look up more cards</a></p>";
-
-// SHOWS CARD IMAGES, BUT IS VERY SLOW TO LOAD
-/*
-for ($i = 0; $i < $num_results; $i++)
-{
-	$row = $result->fetch_assoc();	
-	echo '<img src="'.htmlspecialchars(stripslashes($row['image_url'])).'">';
-	
-	if ($i % 6 === 5 && $i > 0)
-	{
-		echo "<br />";
-	}	
-}
-*/
 
 echo "<br />";
 $result->free();
 $db->close();
 
 
-function construct_results_table($result)
-{
+
+function construct_query($hero, $searchterm, $mana, $mana_operand, $attack, $attack_operand, $health, $health_operand, $race) {
+
+	$query = "select name, hero, type, quality, race, expansion, mana, attack, health from cards where collectable = 1 and type in ('minion', 'spell', 'weapon')";	
+	
+	if (strlen($searchterm) > 0)
+	{
+		$query .= " and name like '%$searchterm%'";
+	}
+	
+	if ($hero != "all")
+	{
+		$query .= " and hero = '$hero'";
+	}
+	
+	if ($race != "all")
+	{
+		$query .= " and race = '$race'";
+	}
+	
+	if (!empty ($mana))
+	{
+		$query .= " and mana $mana_operand $mana";
+	}
+	
+	if (!empty ($attack))
+	{
+		$query .= " and attack $attack_operand $attack";
+	}
+
+	if (!empty ($health))
+	{
+		$query .= " and health $health_operand $health";
+	}
+	
+	
+	return $query;
+}
+
+
+function construct_results_table($result)  {
+
 	$num_results = $result->num_rows;
 	echo "<p>$num_results cards found  |  <a href=\"cardlookup.php\">Look up more cards</a></p>";
 	
@@ -138,6 +114,7 @@ function construct_results_table($result)
 		$row = $result->fetch_assoc();
 		$color = determine_quality($row['quality']);
 		
+		//alternates color in table for readability
 		if ($i % 2 === 1) 
 		{
 			echo "<tr bgcolor=\"#E2E1DF\">";
@@ -152,17 +129,16 @@ function construct_results_table($result)
 		echo "<td>".$row['type']."</td>";
 		echo "<td>".$row['race']."</td>";
 		echo "<td>".$row['expansion']."</td>";
-		echo "<td>".$row['mana']."</td>";
-		echo "<td>".$row['attack']."</td>";
-		echo "<td>".$row['health']."</td>";
+		echo "<td><font color=\"#0000FF\">".$row['mana']."</font></td>";
+		echo "<td><font color=\"#6A6A6A\">".$row['attack']."</font></td>";
+		echo "<td><font color=\"#FF0000\">".$row['health']."</font></td>";
 		echo "</tr>";
 	}	
 	
 	echo "</table>";
 }
 
-function determine_quality($quality)
-{
+function determine_quality($quality) {
 	switch($quality)
 	{
 		case "rare":
@@ -172,7 +148,7 @@ function determine_quality($quality)
 			$color = "#FF00FF";
 			break;
 		case "legendary":
-			$color = "#F0AF26";
+			$color = "#CA8906";
 			break;
 		default:
 			$color = "#000000";
